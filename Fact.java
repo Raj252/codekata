ca76ac671fba4a7216718c0e8bb2065a4de45112
Rajasekar V <47376271+Raj252@users.noreply.github.com>
2019-03-08 18:36:14 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;  
+
+ class fact 
+ {  
+   public static void main(String[] args) 
+   {  
+       Scanner s = new Scanner(System.in);  
+        
+       int number = s.nextInt();  
+       int i,fact=1;  
+    
+  for(i=1;i<=number;i++)
+  {    
+      fact=fact*i;    
+  }    
+  System.out.println(fact);  
+ 
+ }
+}  
